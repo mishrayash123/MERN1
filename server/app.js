fa6f04137e1c6137f6484af8647ba075948a012a
yash kumar mishra <mishrayash3778@gmail.com>
2022-07-06 17:17:34 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,44 @@
+const dotenv = require('dotenv');
+const mongoose =require('mongoose');
+const express =require('express');
+const app =express();
+
+dotenv.config({ path: './config.env' });
+
+require('./db/conn');
+
+const PORT =process.env.PORT;
+
+
+
+//mongodb+srv://mishra:<password>@cluster0.yrprxjp.mongodb.net/?retryWrites=true&w=majority
+
+//middleware
+
+const middleware = (req,res,next) => {
+    console.log('middleware');
+    next();
+}
+
+
+app.get('/',(req, res)=>{
+    res.send('hello Home');
+});
+
+app.get('/about',middleware,(req, res)=>{
+    res.send('hello About');
+});
+
+app.get('/login',(req, res)=>{
+    res.send('hello Login');
+});
+
+app.get('/signup',(req, res)=>{
+    res.send('hello Signup');
+});
+
+app.listen(PORT, ()=> {
+    console.log(`server is running at port no ${PORT}`);
+})
+
+
---
+++ server/db/conn.js
@@ -0,0 +1,11 @@
+const mongoose =require('mongoose');
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB/*, {
+    useNewUrlParser: true,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+}*/).then(() => {
+    console.log('connected successfully');
+}).catch((err) => console.log('not connected'));
