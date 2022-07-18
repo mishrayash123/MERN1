12ff41d2a241f3f0c989596c22ee80278ab16f49
yash kumar mishra <mishrayash3778@gmail.com>
2022-07-18 13:14:05 +0530

    sync await

--- server/router/auth.js
+++ server/router/auth.js
@@ -9,6 +9,8 @@ router.get('/',(req, res)=>{
     res.send('hello home from router js');
 });
 
+//by promises
+/*
 router.post('/signup',(req,res) => {
 const {name , email, phone, work, password, cpassword } = req.body;
 
@@ -35,5 +37,40 @@ User.findOne({email:email}).then((userExist) => {
     // res.json({message : req.body});
 
 });
+*/
+
+// by async and await
+router.post('/signup', async (req,res) => {
+    const {name , email, phone, work, password, cpassword } = req.body;
+    
+    if(!name || !email || !phone || !work || !password || !cpassword ){
+        return res.status(422).json({error: "plz fill all fields"});
+    }
+
+    try{
+       const userExist = await User.findOne({email:email});
+       if(userExist){
+        return res.status(422).json({error: "email already exist"});
+    }
+
+    const user = new User({name , email, phone, work, password, cpassword });
+
+    await user.save();
+
+    res.status(201).json({message: "user regstered successfully"});
+   
+        
+    }catch (err){
+        console.log(err);
+    }
+    
+    
+    
+        // console.log(name);
+        // console.log(email);
+        // res.send('mera register page');
+        // res.json({message : req.body});
+    
+    });
 
 module.exports = router;
