4b58371ca3abe2cc1650289226160c56536a0620
yash kumar mishra <mishrayash3778@gmail.com>
2022-07-19 13:56:33 +0530

    cookies

--- server/model/userSchema.js
+++ server/model/userSchema.js
@@ -1,33 +1,66 @@
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
-    name : {
+    name: {
         type: String,
-        required : true
+        required: true
     },
-    email : {
+    email: {
         type: String,
-        required : true
+        required: true
     },
-    phone : {
+    phone: {
         type: Number,
-        required : true
+        required: true
     },
-    work : {
+    work: {
         type: String,
-        required : true
+        required: true
     },
-    password : {
+    password: {
         type: String,
-        required : true
+        required: true
     },
     cpassword: {
         type: String,
-        required : true
-    }
+        required: true
+    },
+    tokens: [
+        {
+            token: {
+                type: String,
+                required: true
+            }
+        }
+    ]
 })
 
-const User = mongoose.model('USER',userSchema);
+
+// hashing of password
+
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 12);
+        this.cpassword = await bcrypt.hash(this.cpassword, 12);
+    }
+    next();
+});
+
+// we are generating token
+userSchema.methods.generateAuthToken = async function () {
+    try {
+        let token = jwt.sign({
+            _id: this._id
+        }, process.env.SECRET_KEY);
+        this.tokens = this.tokens.concat({token: token});
+        await this.save();
+        return token;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const User = mongoose.model('USER', userSchema);
 
 module.exports = User;
-
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,15 +1,16 @@
+const jwt = require('jsonwebtoken');
 const express = require('express');
 const router = express.Router();
-
+const bcrypt = require('bcryptjs');
 require('../db/conn');
 
-const User =require("../model/userSchema");
+const User = require("../model/userSchema");
 
-router.get('/',(req, res)=>{
+router.get('/', (req, res) => {
     res.send('hello home from router js');
 });
 
-//by promises
+// by promises
 /*
 router.post('/signup',(req,res) => {
 const {name , email, phone, work, password, cpassword } = req.body;
@@ -40,63 +41,95 @@ User.findOne({email:email}).then((userExist) => {
 */
 
 // by async and await
-router.post('/signup', async (req,res) => {
-    const {name , email, phone, work, password, cpassword } = req.body;
-    
-    if(!name || !email || !phone || !work || !password || !cpassword ){
+router.post('/signup', async (req, res) => {
+    const {
+        name,
+        email,
+        phone,
+        work,
+        password,
+        cpassword
+    } = req.body;
+
+    if (!name || !email || !phone || !work || !password || !cpassword) {
         return res.status(422).json({error: "plz fill all fields"});
     }
 
-    try{
-       const userExist = await User.findOne({email:email});
-       if(userExist){
-        return res.status(422).json({error: "email already exist"});
-    }
+    try {
+        const userExist = await User.findOne({email: email});
+        if (userExist) {
+            return res.status(422).json({error: "email already exist"});
+        } else {
+            if (password != cpassword) {
+                return res.status(422).json({error: "password are not matching"});
+            } else {
+                const user = new User({
+                    name,
+                    email,
+                    phone,
+                    work,
+                    password,
+                    cpassword
+                });
 
-    const user = new User({name , email, phone, work, password, cpassword });
+                await user.save();
 
-    await user.save();
-
-    res.status(201).json({message: "user regstered successfully"});
-   
-        
-    }catch (err){
-        console.log(err);
-    }
-    
-    
-    
-        // console.log(name);
-        // console.log(email);
-        // res.send('mera register page');
-        // res.json({message : req.body});
-    
-    });
-
-    //log in route
-
-    router.post('/login', async (req, res) => {
-        // console.log(req.body);
-        // res.json({message : "awesome"});
-
-        try{
-           const { email, password } = req.body;
-           if(!email || !password){
-            return res.status(400).json({error:"plz fill the data"});
-           }
-           const userlogin = await User.findOne({email: email});
-           console.log(userlogin);
-           if(!userlogin){
-            res.status(400).json({error:"user not registered"});
-           }
-           else{
-            res.json({message:"user signed in succesfully"});
-           }
-           
-        }catch(err){
-            console.log(err);
+                res.status(201).json({message: "user regstered successfully"});
+            }
         }
 
-    })
+
+    } catch (err) {
+        console.log(err);
+    }
+
+
+    // console.log(name);
+    // console.log(email);
+    // res.send('mera register page');
+    // res.json({message : req.body});
+
+});
+
+// log in route
+
+router.post('/login', async (req, res) => {
+    // console.log(req.body);
+    // res.json({message : "awesome"});
+
+    try {
+        let token;
+        const {email, password} = req.body;
+        if (!email || !password) {
+            return res.status(400).json({error: "plz fill the data"});
+        }
+        const userlogin = await User.findOne({email: email});
+        // console.log(userlogin);
+
+        if(userlogin){
+            const isMatch = await bcrypt.compare(password,userlogin.password);
+
+            token = await userlogin.generateAuthToken();
+            res.cookie('jwtoken', token, {
+            expires: new Date (Date.now() + 25892000000),
+            httpOnly:true
+            });
+
+            if (! isMatch) {
+                res.status(400).json({error: "invailid crediential"});
+            } else {
+                res.json({message: "user signed in succesfully"});
+            }
+        }
+        else{
+            res.status(400).json({error: "invailid crediential"});
+        }
+        
+
+    } catch (err) {
+        console.log(err);
+    }
+
+})
 
 module.exports = router;
